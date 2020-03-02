4897356a4295b9953e8a49805ab961a806188f5e
felipelvira <felipe.elvira.garcia@outlook.com>
2020-03-02 15:20:56 -0500

    modules

--- CGIAR-QA-back/src/routes/AuthRoute.ts
+++ CGIAR-QA-back/src/routes/AuthRoute.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController";
+import * as checkJwt from "@middlewares/checkJwt";
 // import { checkJwt } from "@middlewares/checkJwt";
-import { checkJwt } from "../middlewares/checkJwt";
+// import { checkJwt } from "../middlewares/checkJwt";
 
 const router = Router();
 //Login route
 router.post("/login", AuthController.login);
 
 //Change my password
-router.post("/change-password", [checkJwt], AuthController.changePassword);
+router.post("/change-password", [checkJwt.checkJwt], AuthController.changePassword);
 
 export default router;
