a6c1337d78b05f9c614b21062ac395729ce02496
felipelvira <felipe.elvira.garcia@outlook.com>
2020-03-04 08:12:12 -0500

    modules updated

--- CGIAR-QA-back/src/routes/IndicatorsRoute.ts
+++ CGIAR-QA-back/src/routes/IndicatorsRoute.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { checkJwt } from "@middleware/checkJwt";
-import { checkRole } from "@middleware/checkRole";
+// import { checkJwt } from "@middleware/checkJwt";
+// import { checkRole } from "@middleware/checkRole";
 // const { checkJwt } = require( "@middlewares/checkJwt");
 // const { checkRole } = require( "@middlewares/checkRole");
+
+import * as checkJwt from "../middlewares/checkJwt";
+import * as checkRole from "../middlewares/checkRole";
 
 import IndicatorsController from "@controllers/IndicatorsController";
 import { RolesHandler } from "@helpers/RolesHandler";
@@ -18,17 +21,17 @@ const router = Router();
  */
 
 // create indicator
-router.post("/", [checkJwt, checkRole([RolesHandler.admin])], IndicatorsController.createIndicator);
+router.post("/", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], IndicatorsController.createIndicator);
 // assing indicator
-router.post("/assign", [checkJwt, checkRole([RolesHandler.admin])], IndicatorsController.assignIndicatorToUser);
+router.post("/assign", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], IndicatorsController.assignIndicatorToUser);
 // get all indicators
-router.get("/", [checkJwt, checkRole([RolesHandler.admin, RolesHandler.assesor, RolesHandler.crp, RolesHandler.guest])], IndicatorsController.getAllIndicators);
+router.get("/", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin, RolesHandler.assesor, RolesHandler.crp, RolesHandler.guest])], IndicatorsController.getAllIndicators);
 // edit indicator
-router.patch("/:id([0-9]+)", [checkJwt, checkRole([RolesHandler.admin])], IndicatorsController.editIndicators);
+router.patch("/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], IndicatorsController.editIndicators);
 // // delete indicator
-router.delete("/:id([0-9]+)", [checkJwt, checkRole([RolesHandler.admin])], IndicatorsController.deleteIndicators);
+router.delete("/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], IndicatorsController.deleteIndicators);
 // get indicator by user
-router.get("/user/:id([0-9]+)", [checkJwt, checkRole([RolesHandler.admin, RolesHandler.assesor, RolesHandler.crp, RolesHandler.guest])], IndicatorsController.getIndicatorsByUser);
+router.get("/user/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin, RolesHandler.assesor, RolesHandler.crp, RolesHandler.guest])], IndicatorsController.getIndicatorsByUser);
 
 
 export default router;
