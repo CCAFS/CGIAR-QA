da6d47edefb0a571300bec070eec78929163a202
felipelvira <felipe.elvira.garcia@outlook.com>
2020-03-04 08:40:15 -0500

    modules updated

--- CGIAR-QA-back/src/routes/UsersRoute.ts
+++ CGIAR-QA-back/src/routes/UsersRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-// import { checkJwt } from "@middleware/checkJwt";
-// import { checkRole } from "@middleware/checkRole";
+import { checkJwt } from "@middleware/checkJwt";
+import { checkRole } from "@middleware/checkRole";
+
 // const { checkJwt } = require( "@middlewares/checkJwt");
 // const { checkRole } = require( "@middlewares/checkRole");
 
-import * as checkJwt from "../../src/middlewares/checkJwt";
-import * as checkRole from "../../src/middlewares/checkRole";
+// import * as checkJwt from "../../src/middlewares/checkJwt";
+// import * as checkRole from "../../src/middlewares/checkRole";
 
 import UserController from "@controllers/UserController";
 import { RolesHandler } from "@helpers/RolesHandler";
@@ -20,31 +21,48 @@ const router = Router();
  */
 
 //Get all users
-router.get("/all", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.listAll);
+router.get("/all", [checkJwt, checkRole([RolesHandler.admin])], UserController.listAll);
+// router.get("/all", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.listAll);
 
 // Get one user
 router.get(
     "/:id([0-9]+)",
-    [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])],
+    [checkJwt, checkRole([RolesHandler.admin])],
     UserController.getOneById
 );
+// router.get(
+//     "/:id([0-9]+)",
+//     [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])],
+//     UserController.getOneById
+// );
 
 //Create a new user
-router.post("/", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.newUser);
+router.post("/", [checkJwt, checkRole([RolesHandler.admin])], UserController.newUser);
+// router.post("/", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.newUser);
 
 //Edit one user
 router.patch(
     "/:id([0-9]+)",
-    [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])],
+    [checkJwt, checkRole([RolesHandler.admin])],
     UserController.editUser
 );
+// router.patch(
+//     "/:id([0-9]+)",
+//     [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])],
+//     UserController.editUser
+// );
 
 //Delete one user
 router.delete(
     "/:id([0-9]+)",
-    [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])],
+    [checkJwt, checkRole([RolesHandler.admin])],
     UserController.deleteUser
 );
+// router.delete(
+//     "/:id([0-9]+)",
+//     [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])],
+//     UserController.deleteUser
+// );
 
 
 /***
@@ -54,13 +72,17 @@ router.delete(
  */
 
 // create role
-router.post("/role", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.createRole);
+router.post("/role", [checkJwt, checkRole([RolesHandler.admin])], UserController.createRole);
+// router.post("/role", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.createRole);
 // get roles
-router.get("/role", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.getAllRoles);
+router.get("/role", [checkJwt, checkRole([RolesHandler.admin])], UserController.getAllRoles);
+// router.get("/role", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.getAllRoles);
 // edit role
-router.patch("/role/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.editRole);
+router.patch("/role/:id([0-9]+)", [checkJwt, checkRole([RolesHandler.admin])], UserController.editRole);
+// router.patch("/role/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.editRole);
 // delete role
-router.delete("/role/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.deleteRole);
+router.delete("/role/:id([0-9]+)", [checkJwt, checkRole([RolesHandler.admin])], UserController.deleteRole);
+// router.delete("/role/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.deleteRole);
 
 /***
  * 
@@ -68,13 +90,17 @@ router.delete("/role/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([Role
  * 
  */
 // create permissions
-router.post("/permission", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.createPermission);
+router.post("/permission", [checkJwt, checkRole([RolesHandler.admin])], UserController.createPermission);
+// router.post("/permission", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.createPermission);
 // get permissions
-router.get("/permission", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.getAllPermissions);
+router.get("/permission", [checkJwt, checkRole([RolesHandler.admin])], UserController.getAllPermissions);
+// router.get("/permission", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.getAllPermissions);
 // delete role
-router.delete("/permission/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.deletePermissions);
+router.delete("/permission/:id([0-9]+)", [checkJwt, checkRole([RolesHandler.admin])], UserController.deletePermissions);
+// router.delete("/permission/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.deletePermissions);
 // edit role
-router.patch("/permission/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.editPermissions);
+router.patch("/permission/:id([0-9]+)", [checkJwt, checkRole([RolesHandler.admin])], UserController.editPermissions);
+// router.patch("/permission/:id([0-9]+)", [checkJwt.checkJwt, checkRole.checkRole([RolesHandler.admin])], UserController.editPermissions);
 
 
 
