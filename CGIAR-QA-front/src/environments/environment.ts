556c9f079dca4eb445a0934b73e4a46bf75bcc24
felipelvira <felipe.elvira.garcia@outlook.com>
2020-03-25 11:31:41 -0500

    server update

--- CGIAR-QA-front/src/environments/environment.ts
+++ CGIAR-QA-front/src/environments/environment.ts
@@ -4,11 +4,12 @@
 
 export const environment = {
   production: false,
-  apiUrl: `http://${window.location.hostname}:8800`
+  apiUrl: `http://localhost:8800`
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
+apiUrl: `http://${window.location.hostname}:8800`
+* For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
