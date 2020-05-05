57a557923ff1e43cdce7dc504d3b814d7dfb9dc8
felipelvira <felipe.elvira.garcia@outlook.com>
2020-05-05 16:18:54 -0500

    May 5: - active directory

--- CGIAR-QA-back/src/config/config.ts
+++ CGIAR-QA-back/src/config/config.ts
@@ -2,5 +2,10 @@ export default {
     jwtSecret: "@QEGTUI",
     port: 8800,
     jwtTime: '5h',
-    host:'0.0.0.0'
+    host: '0.0.0.0',
+    active_directory: {
+        url: 'ldap://ciatroot3.cgiarad.org:3268',
+        baseDN: 'DC=CGIARAD, DC=ORG',
+        domain: "CGIARAD.ORG"
+    }
 };
