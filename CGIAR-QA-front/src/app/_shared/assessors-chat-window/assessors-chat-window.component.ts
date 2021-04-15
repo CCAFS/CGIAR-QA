22288ccff5548633be8eeaff31a4b5e40b28486c
Santiago Galvez <santiagogalvez17@gmail.com>
2021-04-15 15:58:30 -0500

    Updated assessors-chat always active

--- CGIAR-QA-front/src/app/_shared/assessors-chat-window/assessors-chat-window.component.ts
+++ CGIAR-QA-front/src/app/_shared/assessors-chat-window/assessors-chat-window.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-assessors-chat-window',
@@ -8,14 +10,33 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class AssessorsChatWindowComponent implements OnInit {
 
-  @Input() currentUser;
-  
+  // @Input() currentUser;
+  currentUser;
+  currentRole;
   chatRooms = null;
 
   assessorsChat = {
     isOpen: false
   }
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer,
+    private activeRoute: ActivatedRoute,
+    private authenticationService: AuthenticationService) {
+      this.activeRoute.params.subscribe(routeParams => {
+        console.log(routeParams);
+        this.authenticationService.currentUser.subscribe(x => {
+          this.currentUser = x;
+          if (x) {
+            this.currentRole = x.roles[0].description.toLowerCase();
+            console.log(this.currentUser);
+            
+            this.ngOnInit();
+            // this.getHeaderLinks();
+            // this.isHome = `/dashboard/${this.currentUser}`;
+            // this.isHome = this.router.isActive( `/dashboard/${this.currentUser}` , true)
+          }
+        });
+      })
+    }
 
   ngOnInit() {
     console.log('ASSESSORS CHAT');
