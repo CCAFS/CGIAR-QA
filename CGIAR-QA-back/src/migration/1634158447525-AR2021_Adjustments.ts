e3073750d9ba6f1e0aeeb6cff530d971e5e8e10e
Santiago Galvez <santiagogalvez17@gmail.com>
2021-10-13 16:04:10 -0500

    Add AR 2021 adjustments migration & remove additional contribution from SLOs

---
+++ CGIAR-QA-back/src/migration/1634158447525-AR2021_Adjustments.ts
@@ -0,0 +1,17 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AR2021Adjustments1634158447525 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        //SLO
+        await queryRunner.query("UPDATE `marlodb`.`qa_indicators_meta` SET `include_detail` = '0', `order` = '0' WHERE (`id` = '151')");
+
+        //Policies
+        await queryRunner.query("UPDATE `marlodb`.`qa_indicators_meta` SET `display_name` = 'Stage in process' WHERE (`id` = '79')");
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
