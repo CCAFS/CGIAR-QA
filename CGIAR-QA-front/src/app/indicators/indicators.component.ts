450d1a40f3082376ebf340e9cef0cf8d9b9306fd
Santiago Galvez <santiagogalvez17@gmail.com>
2021-11-22 12:10:26 -0500

    Disable next batches

--- CGIAR-QA-front/src/app/indicators/indicators.component.ts
+++ CGIAR-QA-front/src/app/indicators/indicators.component.ts
@@ -135,7 +135,9 @@ indicatorTypePage = null;
 
       const batches = res.data;
       for (let index = 0; index < batches.length; index++) {
-        let batch = {date: moment(batches[index].submission_date).format('ll'), batch_name: +batches[index].batch_name, checked: false};
+        let batch = {date: moment(batches[index].submission_date).format('ll'), batch_name: +batches[index].batch_name, checked: false, is_active: null};
+        batch.is_active = moment(Date.now()).isSameOrAfter(batch.date) || index === 0 ? true : false;
+        batch.checked = batch.is_active;
         this.submission_dates.push(batch);
       }
       
